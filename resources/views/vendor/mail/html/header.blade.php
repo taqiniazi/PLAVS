bc89594e880458b0ff6e4eb4fd16e9d96a6665bc
Taqi Raza <taqi.raza@pitb.gov.pk>
2026-01-23 17:56:54 +0500

    Update header.blade.php

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-<img src="{{ asset('images/logo.png') }}" class="logo" alt="{{ config('app.name') }}" style="max-height: 75px; width: auto;">
+<img src="http://triconv.com/images/logo.png" class="logo" alt="{{ config('app.name') }}" style="max-height: 75px; width: auto;">
 </a>
 </td>
 </tr>
