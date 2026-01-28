103679f519ecdff64a62a0e62d7d6175abba1d34
Taqi Raza <taqi.raza@pitb.gov.pk>
2026-01-28 12:47:40 +0500

    updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LibraFlow | Digital Library Management Reimagined</title>
+    <title>MyBookShelf | Physical Library Management Reimagined</title>
     <!-- Bootstrap 5 CSS -->
     <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
     <!-- Bootstrap Icons -->
